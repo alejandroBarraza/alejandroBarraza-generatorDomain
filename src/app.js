d95bc08078cd0617b5527faab55c383bb24b5f61
alejandroBarraza <alejandro.barraza@alumnos.ucn.cl>
2023-10-04 10:24:03 -0300

    feature(app.js) : add domoain generator function

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function app() {
+  let pronoun = ['the', 'our']
+  let adj = ['great', 'big']
+  let noun = ['jogger', 'racoon']
+  let domain = ['com', 'us', 'cl']
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  for (const p of pronoun) {
+    for (const a of adj) {
+      for (const n of noun) {
+        for (const d of domain) {
+          console.log(`${p}${a}${n}.${d}`)
+        }
+      }
+    }
+  }
+}
+
+app()
